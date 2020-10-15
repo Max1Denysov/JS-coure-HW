06fa7b5a3d15dd1204146d15c578977b615ac49e
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-16 01:56:30 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,6 +1,5 @@
 var s = prompt("Расстояние.km");
-     
 var t = prompt(" желаемое время в пути.Hours");
-var v = s/t
-
-alert('требуемая скорость равна ='+ ' ' +  v+ ' ' +'km/h' );
+var v = s/t;
+var ss = '  ';
+alert('требуемая скорость равна ='+ ss +  v+ ss +'km/h' );
