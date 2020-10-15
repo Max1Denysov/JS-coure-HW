09c63e580af603d000d3f9a0cc48c88e355292ac
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-16 02:05:37 +0300

    Update script7.js

--- script7.js
+++ script7.js
@@ -1,4 +1,5 @@
 var USB = prompt("Размер флешки Гб");
 const file =820;
-var prop = (USB*1000)/file;
-alert(prop+' '+'file/s' );
+var prop = Math.trunc((USB*1000)/file);
+var s = ' ';
+alert('На флешке поместиться'+s+prop+s+'файл-а');
