7a4837fe4c25c55e5437b5e00cab21b6bc24a047
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-16 02:07:05 +0300

    Update script8.js

--- script8.js
+++ script8.js
@@ -1,9 +1,6 @@
 var FMoney = prompt("Cумма денег в кошельке");
 var OPrice = prompt("Цена одной шоколадки");
-
-     
- var Numb = Math.trunc(FMoney/OPrice);
+var Numb = Math.trunc(FMoney/OPrice);
 var s = '   ';
 var oth= FMoney-(OPrice*Numb) ;
- 
 alert(Numb+ s+'шоколадок'+s+'Сдача ='+s+oth);
