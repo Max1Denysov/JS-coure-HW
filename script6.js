dcc2501dad0084a19376b117920c4873a289129c
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-16 01:59:23 +0300

    Update script6.js

--- script6.js
+++ script6.js
@@ -1,7 +1,8 @@
 var USD = prompt("$");
-     
 const uah =  28.36;
 const euro = 0.85;
 var UA = USD*uah;
 var evr = USD*euro;
-alert(UA+' ' +'Гривен'+' / ' +evr +'  '+'Евро ' );
+var s = '  ';
+var ss = ' / ';
+alert(UA+s +'Гривен'+ss +evr +s+'Евро ' );
