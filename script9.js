26e5efdd9401ce0bc54110bfe1e94706e6747df8
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-16 11:59:40 +0300

    Update script9.js

--- script9.js
+++ script9.js
@@ -1,7 +1,2 @@
-var n1 =prompt('Первая цифра');
-var n2 =prompt('Вторая цифра');
-var n3 =prompt('Третья цифра');
-var n123 = n1+n2+n3;
-var n321 = n3+n2+n1;
-var s = '  ';
-alert('ваше число'+s+n123+s+'<=>'+s +'результат'+s+ n321);
+const number =prompt('Введите трехзначное число:')
+alert(('' + number).split('').reverse().join(''))
