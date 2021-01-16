032e91caf126c2f3907b7669f0e31ff90bf21a24
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-01-16 20:59:49 +0200

    Delete script8.js

--- script8.js
+++
@@ -1,6 +0,0 @@
-let money = prompt('Cумма денег в кошельке');
-let price = prompt('Цена одной шоколадки');
-let numb = Math.trunc(money / price);
-let oth = money - (price * numb) ;
-let res = numb + '/' + oth;
-alert(' шоколадок/Сдача =' + res);
