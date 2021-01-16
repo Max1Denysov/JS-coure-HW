0fc398a7e04726352ef68b60fab614bc5fa2a0ce
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-01-16 20:45:54 +0200

    Create HW_1.js

---
+++ HW_1.js
@@ -0,0 +1,26 @@
+//Task № 1
+let put = ['hello'];
+put[2] = prompt('enter name');
+put[1] = prompt('enter surname');
+alert(put);
+//Task № 2 
+function myAge() {
+  let year = 2020 - prompt('год рождения')
+  return  year
+}
+ alert( myAge())
+//Task № 3
+let side = prompt('Длина стороны');
+let sq = side ** 2;
+let pr = side * 4;
+let g = sq + ' / ' + pr;
+alert('площадь / периметр =' +  g);
+//Task № 4
+let r = prompt('Радиус окружности');
+let p =   Math.PI * r ** 2;
+alert('площадь =' +  p);
+//Task № 5
+//Task № 6
+//Task № 7
+//Task № 8
+//Task № 9
